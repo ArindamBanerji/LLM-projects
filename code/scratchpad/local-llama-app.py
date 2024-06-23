3e73977d95f1f8eb7df3f57cce3b295150e79526
ArindamBanerji <banerji.arindam@gmail.com>
2024-06-22 20:39:05 -0700

    Restored clean directory

---
+++ code/scratchpad/local-llama-app.py
@@ -0,0 +1,46 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+import streamlit as st
+import os
+
+from dotenv import load_dotenv, find_dotenv, dotenv_values
+import os
+
+env_file = find_dotenv(".env.dev")
+load_dotenv(env_file)
+config = dotenv_values(env_file)
+
+# Warning control
+import warnings
+warnings.filterwarnings("ignore")
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+## Prompt Template
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please response to the user queries"),
+        ("user","Question:{question}")
+    ]
+)
+## streamlit framework
+
+st.title('Langchain Demo With LLAMA2 API')
+input_text=st.text_input("Search the topic u want")
+
+# ollama LLAma2 LLm 
+llm=Ollama(model="llama2")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
+
+
